179c2c70a5f181ae47194c2c6dd93f1066e14ef2
lisethbustos65-tech <lisethbustos65@gmail.com>
2026-06-16 06:05:53 -0500

    Create CicloWhile.java

---
+++ codigo/codigo/CicloWhile.java
@@ -0,0 +1,12 @@
+public class CicloWhile {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while(i <= 10){
+            System.out.println("Número: " + i);
+            i++;
+        }
+
+    }
+}

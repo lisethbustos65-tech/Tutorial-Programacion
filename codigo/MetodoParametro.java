0739ee993ab58b5f86a9283d52631262fde2e559
lisethbustos65-tech <lisethbustos65@gmail.com>
2026-06-16 06:10:49 -0500

    Create MetodoParametro.java

---
+++ codigo/MetodoParametro.java
@@ -0,0 +1,15 @@
+public class MetodoParametros {
+
+    public static void mostrarMensaje(String nombre) {
+
+        System.out.println("Bienvenido " + nombre);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        mostrarMensaje("Liseth");
+
+    }
+}

4e2ee4f1de67423305bc3ddedb251ebf94673c1a
lisethbustos65-tech <lisethbustos65@gmail.com>
2026-06-16 06:11:12 -0500

    Create MetodoRetorno.java

---
+++ codigo/MetodoRetorno.java
@@ -0,0 +1,18 @@
+public class MetodoRetorno {
+
+
+    public static int sumar(int a, int b) {
+
+        return a + b;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int resultado = sumar(8, 5);
+
+        System.out.println("La suma es: " + resultado);
+
+    }
+}

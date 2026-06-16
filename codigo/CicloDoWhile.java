4bd9e7223bd6ca6e1a2844ae489cabc1f3dfc44a
lisethbustos65-tech <lisethbustos65@gmail.com>
2026-06-16 06:10:04 -0500

    Create CicloDoWhile.java

---
+++ codigo/CicloDoWhile.java
@@ -0,0 +1,14 @@
+public class CicloDoWhile {
+
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        do {
+            System.out.println("Número: " + numero);
+            numero++;
+
+        } while (numero <= 10);
+
+    }
+}

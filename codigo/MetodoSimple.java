da3afdeef6c8c1f49b85cd71ac5bf404772382f7
lisethbustos65-tech <lisethbustos65@gmail.com>
2026-06-16 06:10:30 -0500

    Create MetodoSimple.java

---
+++ codigo/MetodoSimple.java
@@ -0,0 +1,15 @@
+public class MetodoSimple {
+
+    public static void saludar() {
+
+        System.out.println("Hola, este es un método simple en Java");
+
+    }
+
+
+    public static void main(String[] args) {
+
+        saludar();
+
+    }
+}

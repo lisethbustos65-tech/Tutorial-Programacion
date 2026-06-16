c2313e501f0b65c12497b320215b1cecbd9183e5
lisethbustos65-tech <lisethbustos65@gmail.com>
2026-06-16 06:11:29 -0500

    Create Vector.java

---
+++ codigo/Vector.java
@@ -0,0 +1,15 @@
+public class Vector {
+
+    public static void main(String[] args) {
+
+        int vector[] = {10, 20, 30, 40, 50};
+
+
+        for(int i = 0; i < vector.length; i++) {
+
+            System.out.println("Posición " + i + ": " + vector[i]);
+
+        }
+
+    }
+}
